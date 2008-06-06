7349eec8ac819bb39c67e93b7b7fdaa295cc95c0
doryphores <doryphores@gmail.com>
2008-06-06 13:12:36 +0000

    Tagged 2.0 beta 1

---
+++ audio-player/tags/2.0b1/plugin/assets/audio-player.js
@@ -0,0 +1,47 @@
+var AudioPlayer = {
+    setup : function (playerURL, defaultOptions) {
+        this.playerURL = playerURL;
+        this.defaultOptions = defaultOptions;
+    },
+    
+    embed : function (elementID, options) {
+        var instanceOptions = {};
+        var key;
+        var so;
+		var bgcolor;
+		var wmode;
+		
+		var flashParams = {};
+		var flashVars = {};
+		var flashAttributes = {};
+
+        // Merge default options and instance options
+		for (key in this.defaultOptions) {
+            instanceOptions[key] = this.defaultOptions[key];
+        }
+        for (key in options) {
+            instanceOptions[key] = options[key];
+        }
+        
+		if (instanceOptions.transparentpagebg == "yes") {
+			flashParams.bgcolor = "#FFFFFF";
+			flashParams.wmode = "transparent";
+		} else {
+			flashParams.bgcolor = "#" + instanceOptions.pagebg;
+			flashParams.wmode = "opaque";
+		}
+
+		flashParams.menu = "false";
+		
+        for (key in instanceOptions) {
+			if (key == "pagebg" || key == "width" || key == "transparentpagebg") {
+				continue;
+			}
+            flashVars[key] = instanceOptions[key];
+        }
+
+		flashAttributes.id = elementID.replace("-", "_") + "_player";
+
+        swfobject.embedSWF(this.playerURL, elementID, instanceOptions.width.toString(), "24", "6.0.0", false, flashVars, flashParams, flashAttributes);
+    }
+};
